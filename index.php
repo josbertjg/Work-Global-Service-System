f09463cdad470772d7e73ca2dc8b086167ed1e04
JOSBERT GUEDEZ <josbertjg@gmail.com>
2024-04-09 16:55:31 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php  
+ 
+  session_start();
+
+  if(file_exists('vendor/autoload.php')) {
+    require 'vendor/autoload.php';
+  }else{
+    return "Error: no se encontró el autoload.";
+  }
+
+  Use config\componentes\configSistema as configSistema;
+
+  $GlobalConfig = new configSistema();
+  $GlobalConfig->_int();
+
+  Use bin\controladores\frontControlador as frontControlador;
+
+  $IndexSystem = new frontControlador($_REQUEST);
+
+?>
